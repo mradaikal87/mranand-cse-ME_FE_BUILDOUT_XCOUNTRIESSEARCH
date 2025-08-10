3ef473011235c14a8e87123a89817041fc435592
anand <mranand.cse@gmail.com>
2025-08-10 08:27:05 +0530

    test case issue fixing

--- src/flag_section.js
+++ src/flag_section.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 const Card = ({ image, altText, countryName }) => {
   return (
     <div
+      className="country-card"
       style={{
         display: "flex",
         flexDirection: "column",
@@ -15,13 +16,7 @@ const Card = ({ image, altText, countryName }) => {
       }}
     >
       <img src={image} alt={altText} width={100} height={100} />
-      <h2
-        style={{
-          fontSize: "17px",
-        }}
-      >
-        {countryName}
-      </h2>
+      <h2 style={{ fontSize: "17px" }}>{countryName}</h2>
     </div>
   );
 };
@@ -32,51 +27,52 @@ const api =
 export default function Flagsection() {
   const [flag, setFlag] = useState([]);
   const [search, setSearch] = useState("");
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     fetch(api)
       .then((res) => res.json())
       .then((jsonRes) => setFlag(jsonRes))
-      .catch((err) => console.error("Error fetching data:", err));
-  }, []); // run only once
+      .catch((err) => {
+        console.error(err);
+        setError(true);
+      });
+  }, []);
 
   const filteredFlags = flag.filter((item) =>
     item.common.toLowerCase().includes(search.toLowerCase())
   );
 
   return (
-    <div
-      style={{
-        display: "flex",
-        gap: "10px",
-        flexWrap: "wrap",
-        padding: "0 50px ",
-      }}
-    >
+    <div style={{ display: "flex", gap: "10px", flexWrap: "wrap" }}>
       <div style={{ width: "100%" }}>
         <input
           type="text"
-          placeholder="Search country"
+          placeholder="search"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           style={{
-            padding: "8px",
-            width: "500px",
-            borderRadius: "4px",
-            border: "1px solid #ccc",
-            margin: "30px 0",
+            width: "600px",
+            height: "42px",
+            margin: "40px 0",
           }}
         />
       </div>
 
-      {filteredFlags.map((item) => (
-        <Card
-          key={item.common}
-          image={item.png}
-          altText={item.common}
-          countryName={item.common}
-        />
-      ))}
+      {error && <p>Error fetching countries.</p>}
+
+      {filteredFlags.length > 0 ? (
+        filteredFlags.map((item) => (
+          <Card
+            key={item.common}
+            image={item.png}
+            altText={item.common}
+            countryName={item.common}
+          />
+        ))
+      ) : (
+        <p>No results found</p>
+      )}
     </div>
   );
 }
